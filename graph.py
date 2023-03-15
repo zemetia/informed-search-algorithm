f508911709521f6b57b96a5e9d188926066080bd
yoel-mc <yoel@mulaicoding.id>
2023-03-15 13:37:59 +0700

    feat: fix graph class logic

--- graph.py
+++ graph.py
@@ -1,22 +1,27 @@
 import json
 
 class Node:
-    def __init__(self, city: str, dist: int) -> None:
+    def __init__(self, city: str, heur: int = 0) -> None:
         self.city: str = city
+        self.heur: int = heur
+        self.child = []
+
+class ChildNode:
+    def __init__(self, node: Node, dist: int) -> None:
+        self.node: Node = node
         self.dist: int = dist
 
 class CityGraph:
     def __init__(self) -> None:
         self.map = {}
         self.heuristic = {}
-        self.city = {}
+        self.cities = {}
 
-    def load_data(self, dataset_name: str) -> None:
+    def load_data(self, dataset_name):
         self.map: dict = json.load(open(f'./data/{dataset_name}/map.json'))
         self.heuristic: dict = json.load(open(f'./data/{dataset_name}/heuristic.json'))
 
-        for data in self.heuristic:
-            self.city.setdefault(data['city'], [])
+        self.cities = { city: Node(city, heur) for _, (city, heur) in enumerate(self.heuristic.items()) }
         
         self.create_adj_list()
 
@@ -25,8 +30,8 @@ class CityGraph:
             return
         
         for data in self.map:
-            self.city[data['city1']].append(Node(data['city2'], data['distance']))
-            self.city[data['city2']].append(Node(data['city1'], data['distance']))
+            self.cities[data['city1']].child.append(ChildNode(self.cities[data['city2']], data["distance"]))
+            self.cities[data['city2']].child.append(ChildNode(self.cities[data['city1']], data["distance"]))
 
             
 
